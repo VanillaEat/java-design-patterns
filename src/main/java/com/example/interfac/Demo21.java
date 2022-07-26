ac56c8ba544dc6080620419ca30f8b8d1943a96e
liuyuanyuan <liuyuanyuan@pccpa.cn>
2022-07-26 14:37:32 +0800

    1

---
+++ src/main/java/com/example/interfac/Demo21.java
@@ -0,0 +1,18 @@
+package com.example.interfac;
+
+/**
+ * 功能描述
+ *
+ * @author: scott
+ * @date: 2022年07月26日 14:32
+ */
+public class Demo21 {
+    /**
+     * 功能描述
+     *抽象类与接口的三点区别：
+     * 1.一个是类，一个是接口，声明一个是class ，一个是interface
+     * 2.接口中只有方法，类中有字段和方法
+     * 3.抽象类中的方法是抽象方法，接口中也是抽象方法。如何使用抽象类与接口。一个是继承于extends抽象类，一个是实现implements于接口
+     */
+    
+}
